d3b9c21b2925f413376700f98e2aba3012fa6f5a
danaklv <kalykovadana3@gmail.com>
2025-11-23 01:36:11 +0500

    sql tables corrected

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,24 +1,26 @@
 CREATE TABLE IF NOT EXISTS teams (
-    team_id SERIAL NOT NULL PRIMARY KEY,
+    team_id SERIAL PRIMARY KEY,
     team_name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS users (
     user_id VARCHAR(50) PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
-    team_id BIGINT NOT NULL REFERENCES teams(team_id) ON DELETE CASCADE
+    team_id INT NOT NULL REFERENCES teams(team_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS pull_requests (
-    pull_request_id SERIAL NOT NULL PRIMARY KEY,
+    pull_request_id VARCHAR(50) PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    author BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-    status VARCHAR(6) NOT NULL CHECK (status IN ('OPEN', 'MERGED'))
+    author VARCHAR(50) NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    status VARCHAR(6) NOT NULL CHECK (status IN ('OPEN', 'MERGED')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    merged_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS reviewers (
-    user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
-    pull_request_id BIGINT REFERENCES pull_requests(pull_request_id) ON DELETE CASCADE,
+    user_id VARCHAR(50) NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    pull_request_id VARCHAR(50) NOT NULL REFERENCES pull_requests(pull_request_id) ON DELETE CASCADE,
     PRIMARY KEY (pull_request_id, user_id)
 );
